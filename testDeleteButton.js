23c7bcc2847d8984c69debda4f3689afb11297fa
andreagonzalezsilva <andreagonzalezs1708@gmail.com>
2024-06-05 16:02:39 -0700

    Add test coverage for the Delete Button feature

---
+++ testDeleteButton.js
@@ -0,0 +1,55 @@
+const {By, Builder, Browser} = require('selenium-webdriver');
+const assert = require("assert");
+const expectedTitle = "DevOps Bonus Project - CCTB";
+const expectedMemberName = "Test, Selenium";
+const expectedMembersAfterDeletion = 0;
+
+(async function deleteButtonTest() {
+  let driver;
+  
+  try {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('https://testing-example2-devops.web.app/');
+
+    // Maxime the window
+    driver.manage().window().maximize();
+
+    // Validate the expected title
+    let title = await driver.getTitle();
+    assert.equal(title, expectedTitle);
+    await driver.manage().setTimeouts({implicit: 500});
+  
+    // Add a new member
+    // Create the web elements 
+    let lastNameField = await driver.findElement(By.id('lastname'));
+    let firstNameField = await driver.findElement(By.id('firstname'));
+    let groupSize = await driver.findElement(By.id('GroupSize'));
+    let addMemberBtn = await driver.findElement(By.id('addMemberBtn'));
+    let deleteMemberBton = await driver.findElement(By.id('deleteMemberBtn'));
+
+    // Create the new member by inputting the first name, last name and group size
+    await lastNameField.sendKeys("Test");
+    await firstNameField.sendKeys("Selenium");  
+    await groupSize.sendKeys("5");  
+    await addMemberBtn.click();
+    
+    // Search for the new created member
+    let createdMember = await driver.findElement(By.xpath('//*[@id="members"]/option'));
+    let createdMemberName = await createdMember.getText();
+
+    // Validate the member has been created
+    assert.equal(createdMemberName, expectedMemberName)
+
+    // Select and delete the new member
+    await createdMember.click();
+    await deleteMemberBton.click();
+
+    // Validate the member has been deleted
+    let foundMembers = await driver.findElements(By.xpath('//*[@id="members"]/option'));
+    assert.equal(foundMembers.length, expectedMembersAfterDeletion);
+  } catch (e) {
+    console.log(e)
+  } finally {
+    await driver.quit();
+  }
+}())
